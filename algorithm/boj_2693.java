28f3d8ccbce9ec154564ff583f57f74170b7cef7
leegangho <gleehave@gmail.com>
2022-11-26 20:23:09 +0900

    [add]

---
+++ algorithm/boj_2693.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class boj_2693 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i=0; i<t;i++){
+            list.clear();
+            for (int j=0; j<10; j++){
+                list.add(sc.nextInt());
+            }
+            Collections.sort(list);
+            System.out.println(list.get(7));
+        }
+    }
+}
