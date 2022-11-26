aecb9d2c1602bca0a2758aaa35b4b861566baf6e
leegangho <gleehave@gmail.com>
2022-11-26 19:45:39 +0900

    [add] 피보나치

---
+++ algorithm/boj_10870.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class boj_10870 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fivo(n));
+    }
+
+    public static int fivo(int n){
+        if(n==0){
+            return n;
+        }
+        else if (n == 1){
+            return 1;
+        }
+
+        return fivo(n-1) + fivo(n-2);
+    }
+}
