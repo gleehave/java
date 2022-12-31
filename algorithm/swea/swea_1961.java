e4b34714cac7c18d4f668f16e83481b9a88530fb
gleehave <gleehave@gmail.com>
2022-12-31 13:57:37 +0900

    [fix]

--- algorithm/swea/swea_1961.java
+++ algorithm/swea/swea_1961.java
@@ -3,51 +3,34 @@ package algorithm.swea;
 import java.util.Scanner;
 
 public class swea_1961 {
-    public static int[][] Rotation(int[][] arr){
-        int[][] result = new int[arr.length][arr.length];
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                result[i][j] = arr[arr.length - j - 1][i];
-            }
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for (int tc = 1; tc <= T; tc++) {
-            int N = sc.nextInt();
-            int[][] arr = new int[N][N];
+        for (int t = 1; t <= T; t++) {
+            int n = sc.nextInt();
+            int[][] a = new int[n][n];
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    arr[i][j] = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = sc.nextInt();
                 }
             }
-
-            int[][] degree_90 = Rotation(arr);
-            int[][] degree_180 = Rotation(degree_90);
-            int[][] degree_270 = Rotation(degree_180);
-
-            System.out.printf("#%d\n", tc);
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    System.out.print(degree_90[i][j]);
+            System.out.println("#" + t + " ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(a[n - j - 1][i]);
                 }
-                System.out.print("");
-
-                for (int j = 0; j < N; j++) {
-                    System.out.print(degree_180[i][j]);
+                System.out.print(" ");
+                for (int j = 0; j < n; j++) {
+                    System.out.print(a[n - i - 1][n - j - 1]);
                 }
-                System.out.print("");
-
-                for (int j = 0; j < N; j++) {
-                    System.out.print(degree_270[i][j]);
+                System.out.print(" ");
+                for (int j = 0; j < n; j++) {
+                    System.out.print(a[j][n - 1 - i]);
                 }
                 System.out.println();
             }
+
         }
     }
 }
