b3e2d0c8011410b7fbb62bd76d7c6e24109aa234
gleehave <gleehave@gmail.com>
2023-01-06 22:48:57 +0900

    [fix]

---
+++ algorithm/swea/swea_3234.java
@@ -0,0 +1,14 @@
+package algorithm.swea;
+
+import java.util.Scanner;
+
+public class swea_3234 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int TC = sc.nextInt();
+
+        for (int tc = 1; tc <= TC; tc++) {
+            // ....
+        }
+    }
+}
