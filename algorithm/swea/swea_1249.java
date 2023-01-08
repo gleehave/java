e61328b66fbacc3ac879ce6d30b7ce16740c570e
gleehave <gleehave@gmail.com>
2023-01-08 17:24:49 +0900

    [fix]

--- algorithm/swea/swea_1249.java
+++ algorithm/swea/swea_1249.java
@@ -1,98 +1,92 @@
 package algorithm.swea;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
-class Position{
-    int x;
-    int y;
-
-    Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+import java.util.*;
+import java.io.*;
 
 public class swea_1249 {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    static int n;
+    static int N;
     static int min;
-
     static int[][] map;
-    static int[][] ans;
-    static boolean[][] visited;
+    static boolean[][] isVisited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws Exception
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testNum = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for (int test = 1; test <= testNum; test++) {
-            n = Integer.parseInt(br.readLine());
+        for(int tc=0; tc<T; tc++) {
+            min = Integer.MAX_VALUE;
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+            isVisited = new boolean[N][N];
 
-            map = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                String[] temp = br.readLine().split("");
-                for (int j = 0; j < n; j++) {
-                    map[i][j] = Integer.parseInt(temp[j]);
+            for(int i=0; i<N; i++) {
+                String s = br.readLine();
+                for(int j=0; j<N; j++) {
+                    map[i][j] = s.charAt(j) - '0';
                 }
             }
 
-            min = Integer.MIN_VALUE;
-            visited = new boolean[n][n];
-
-            ans = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                Arrays.fill(ans[i], Integer.MAX_VALUE);
-                ans[0][0] = 0;
-
-                bfs(0, 0);
-                System.out.println("#"+test+" "+min);
-            }
-            br.close();
+            bfs(0, 0);
+            System.out.println("#" + (tc+1) + " "+min);
         }
     }
 
-    public static void bfs(int x, int y) {
-        Queue<Position> queue = new LinkedList<>();
-        queue.offer(new Position(x, y));
-        visited[x][y] = true;
-        while (!queue.isEmpty()) {
-            Position pos = queue.poll();
-            int a = pos.x;
-            int b = pos.y;
+    static void bfs(int x, int y) {
+        PriorityQueue<Pos> que = new PriorityQueue<>();
 
-            if (a == n - 1 && b == n - 1) {
-                min = min > ans[n - 1][n - 1] ? ans[n - 1][n - 1] : min;
+        que.add(new Pos(x, y, 0));
+        isVisited[x][y] = true;
+
+        while(!que.isEmpty()) {
+            Pos p = que.poll();
+
+            int curX = p.x;
+            int curY = p.y;
+            int curT = p.time;
+
+            if(curX == N-1 && curY == N-1) {
+                min = min > curT ? curT : min;
             }
 
-            if (min <= ans[a][b]) {
-                continue;
-            }
+            for(int t=0; t<4; t++) {
+                int nx = curX + dx[t];
+                int ny = curY + dy[t];
 
-            for (int i = 0; i < 4; i++) {
-                int nx = a + dx[i];
-                int ny = b + dy[i];
-                if (isValidPosition(nx, ny)) {
-                    if (!visited[nx][ny] || ans[nx][ny] > ans[a][b] + map[nx][ny]) {
-                        visited[nx][ny] = true;
-                        ans[nx][ny] = ans[a][b] + map[nx][ny];
-                        queue.offer(new Position(nx, ny));
-                    }
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
+                if(!isVisited[nx][ny]) {
+                    int totalTime = curT + map[nx][ny];
+                    que.add(new Pos(nx, ny, totalTime));
+                    isVisited[nx][ny] = true;
                 }
+
             }
 
         }
+
     }
 
-    private static boolean isValidPosition(int x, int y) {
-        if (x < 0 || x >= n || y < 0 || y >= n) {return false;}
-        return true;
-    }
+    static class Pos implements Comparable<Pos>{
+        int x, y;
+        int time;
 
+        Pos(int x, int y, int time){
+            this.x = x;
+            this.y = y;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Pos o) {
+            if(this.time < o.time) {
+                return -1;
+            } else if(this.time > o.time) {
+                return 1;
+            }
+            return 0;
+        }
+    }
 }
