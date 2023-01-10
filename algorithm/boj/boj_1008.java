5c31a691eaabebeb9a501f029b6766bf7b896a50
gleehave <gleehave@gmail.com>
2023-01-10 21:06:44 +0900

    [add]

---
+++ algorithm/boj/boj_1008.java
@@ -0,0 +1,13 @@
+package algorithm.boj;
+
+import java.util.Scanner;
+
+public class boj_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+
+        System.out.println(a/b);
+    }
+}
