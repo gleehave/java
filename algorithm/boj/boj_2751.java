93ccfba1f274ea9da0bc5ac73ddb5298c61781c1
gleehave <gleehave@gmail.com>
2023-01-11 20:30:52 +0900

    [add]

---
+++ algorithm/boj/boj_2751.java
@@ -0,0 +1,46 @@
+package algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class boj_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for (int value : list) {
+            sb.append(value).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+
+    /*
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        Arrays.sort(arr);
+
+        for (int target : arr) {
+            System.out.println(target);
+        }
+    }
+     */
+}
